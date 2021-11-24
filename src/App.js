9c3b8f9eef7eadfbdf56b4b4f9896fc58d83e3d2
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-11-24 19:04:19 -0300

    Criado botao de configuracoes epagina de configuracoes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 // import logo from './trivia.png';
 import Login from './pages/Login';
+import Configurations from './pages/Configurations';
 import './App.css';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/configurations" component={ Configurations } />
       <Route path="/" component={ Login } />
     </Switch>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import setPlayerInfo from '../actions';
 
 class Login extends React.Component {
   constructor() {
@@ -8,11 +11,39 @@ class Login extends React.Component {
       gravatarEmail: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.linkToConfig = this.linkToConfig.bind(this);
+    this.renderConfigButton = this.renderConfigButton.bind(this);
   }
 
   handleChange({ target }) {
     const { name, value } = target;
     this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { playerInfo } = this.props;
+    const { state } = this;
+    playerInfo(state);
+  }
+
+  linkToConfig(event) {
+    event.preventDefault();
+    const { history } = this.props;
+    history.push('./configurations');
+  }
+
+  renderConfigButton() {
+    return (
+      <button
+        type="submit"
+        data-testid="btn-settings"
+        onClick={ this.linkToConfig }
+      >
+        Configurações
+      </button>
+    );
   }
 
   render() {
@@ -47,16 +78,29 @@ class Login extends React.Component {
           <button
             type="submit"
             data-testid="btn-play"
+            onClick={ this.handleSubmit }
             disabled={
               !(name.length > 0 && gravatarEmail.length > 0)
             }
           >
-            Entrar
+            Jogar
           </button>
 
         </form>
+        { this.renderConfigButton() }
       </div>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  playerInfo: (payload) => dispatch(setPlayerInfo(payload)),
+});
+
+Login.propTypes = {
+  playerInfo: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
