ae3f7dbb063af4a142bf8e108017ad601aab14d9
Sayro <sayrodamaceno70@gmail.com>
2021-11-25 13:24:40 -0300

    cria pasta helpers

--- src/pages/GameScreen.js
+++ src/pages/GameScreen.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import html from 'react-inner-html';
 import fetchTrivia from '../services/fetchTrivia';
+import randomAnswers from '../helpers/randomAnswers';
 
-const RANDOM_SUBTRACT = 0.5;
 const QUESTIONS_LENGTH = 4;
 
 class GameScreen extends React.Component {
@@ -14,7 +14,6 @@ class GameScreen extends React.Component {
       index: 0,
     };
     this.renderQuestions = this.renderQuestions.bind(this);
-    this.renderAnswers = this.renderAnswers.bind(this);
   }
 
   componentDidMount() {
@@ -27,42 +26,14 @@ class GameScreen extends React.Component {
     this.setState({ questions: questions.results });
   }
 
-  renderAnswers(question) {
-    const result = [
-      {
-        text: question.correct_answer,
-        tesiId: 'correct-answer',
-      },
-      {
-        text: question.incorrect_answers[0],
-        tesiId: 'wrong-answer-1',
-      },
-    ];
-
-    if (question.incorrect_answers.length > 1) {
-      return [
-        ...result,
-        {
-          text: question.incorrect_answers[1],
-          tesiId: 'wrong-answer-2',
-        },
-        {
-          text: question.incorrect_answers[2],
-          tesiId: 'wrong-answer-3',
-        },
-      ];
-    }
-    return result;
-  }
-
   renderQuestions(question) {
-    const answers = this.renderAnswers(question);
-    const randomAnswers = answers.sort(() => Math.random() - RANDOM_SUBTRACT);
+    const answers = randomAnswers(question);
+
     return (
       <div>
         <h2 data-testid="question-category">{ question.category }</h2>
         <p data-testid="question-text" { ...html(question.question) } />
-        { randomAnswers.map((answer) => (
+        { answers.map((answer) => (
           <button
             data-testid={ answer.tesiId }
             type="button"
